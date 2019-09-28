7462075bfca95da9eb41532301a3077e31d7d90e
HarmonMendelowitz <mendelowitz@gwu.edu>
2019-09-28 14:04:12 -0700

    proc.h made, added procstat to user level

--- user.h
+++ user.h
@@ -1,5 +1,6 @@
 struct stat;
 struct rtcdate;
+struct pstat;
 
 // system calls
 int   fork(void);
@@ -23,6 +24,7 @@ int   getpid(void);
 char *sbrk(int);
 int   sleep(int);
 int   uptime(void);
+int   procstat(uint, struct pstat *);
 
 // ulib.c
 int   stat(char *, struct stat *);
